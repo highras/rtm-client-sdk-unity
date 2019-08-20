fd812556e2b98a942015718bf63b242da44b0fb4
zhao.dx <zhaod1025@gmail.com>
2019-08-20 19:51:11 +0800

    build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@
 # go to project root.
 echo [LiveData] Unity Exporting... {$(pwd)}
 
+# kill unity
+killall Unity
+
 UNITY_PATH="/Applications/Unity/Unity.app/Contents/MacOS/Unity"
 $UNITY_PATH -batchmode -nographics -projectPath $(pwd) -executeMethod ExportPackage.ExportPlugin -quit \
     || die "Failed to export package. Make sure the project is not open in Unity"
