60e0f4515299e85007bdaa1c0efea0febbcafe55
zhao.dx <zhaod1025@gmail.com>
2019-08-20 14:59:17 +0800

    1. add build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# chmod 777 build.sh
+# go to project root.
+echo [LiveData] Exporting... {$(pwd)}
+
+UNITY_PATH="/Applications/Unity/Unity.app/Contents/MacOS/Unity"
+$UNITY_PATH -batchmode -nographics -projectPath $(pwd) -executeMethod ExportPackage.ExportPlugin -quit \
+    || die "Failed to export package. Make sure the project is not open in Unity"
+
+echo [LiveData] Done!
