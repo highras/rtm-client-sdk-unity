317607674cec5be04f4176325ba8d7564d2fd5f4
zhao.dx <zhaod1025@gmail.com>
2019-08-20 18:35:42 +0800

    build.sh

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 # chmod 777 build.sh
 # go to project root.
-echo [LiveData] Exporting... {$(pwd)}
+echo [LiveData] Unity Exporting... {$(pwd)}
 
 UNITY_PATH="/Applications/Unity/Unity.app/Contents/MacOS/Unity"
 $UNITY_PATH -batchmode -nographics -projectPath $(pwd) -executeMethod ExportPackage.ExportPlugin -quit \
     || die "Failed to export package. Make sure the project is not open in Unity"
 
-echo [LiveData] Done!
+echo [LiveData] Export Done!
