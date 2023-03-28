8146794b07ad86b0ac542d574fb301102c706f2c
duo.sun <duo.sun@ilivedata.com>
2023-03-28 14:02:24 +0800

    update version code

--- Assets/Plugins/rtm/RTMConfig.cs
+++ Assets/Plugins/rtm/RTMConfig.cs
@@ -15,7 +15,7 @@ namespace com.fpnn.rtm
 
     public class RTMConfig
     {
-        public static readonly string SDKVersion = "2.7.16";
+        public static readonly string SDKVersion = "2.7.17";
         public static readonly string InterfaceVersion = "2.7.3";
         public static readonly string RTMGameObjectName = "RTM_GAMEOBJECT";
 
