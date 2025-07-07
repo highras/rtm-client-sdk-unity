c24dc7b87c1f75cc79330d711989f90020bd37b5
lingqi.meng <lingqi.meng@ilivedata.com>
2025-07-07 14:27:51 +0800

    rtm unity update 16kb

--- Assets/Plugins/rtm/RTMConfig.cs
+++ Assets/Plugins/rtm/RTMConfig.cs
@@ -10,7 +10,7 @@ namespace com.fpnn.rtm
 
     public class RTMConfig
     {
-        public static readonly string SDKVersion = "2.7.25";
+        public static readonly string SDKVersion = "2.7.26";
         public static readonly string InterfaceVersion = "2.7.3";
         public static readonly string RTMGameObjectName = "RTM_GAMEOBJECT";
 
